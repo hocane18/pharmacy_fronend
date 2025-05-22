1cc9e86882f6951f78d50c065b842d6e32f51c95
Fahad Mujtaba <58139456+hocane18@users.noreply.github.com>
2025-05-22 11:16:38 +0500

    feat(dashboard): add new componen

---
+++ src/app/routes/users/user.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { UserComponent } from './userMangement/user.component';
+
+export const routes: Routes = [{ path: 'user', component: UserComponent }];
